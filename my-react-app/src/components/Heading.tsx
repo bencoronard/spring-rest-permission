4c709b7fac1d9bd71353e3ca4839f5449712a47f
Ben <128577990+bencoronard@users.noreply.github.com>
2025-02-13 19:07:42 +0700

    Restructure repository

--- my-react-app/src/components/Heading.tsx
+++
@@ -1,8 +0,0 @@
-import { ReactElement } from 'react';
-import styles from '../styles/app.module.css';
-
-type HeadingProps = { title: string };
-
-export const Heading = ({ title }: HeadingProps): ReactElement => {
-  return <h1 className={styles.success}>{title}</h1>;
-};
--- my-react-app/src/components/withContext.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const CustomContext = React.createContext('Default');
-
-const CustomContextProvider = CustomContext.Provider;
-const CustomContextConsumer = CustomContext.Consumer;
-
-export { CustomContextProvider, CustomContextConsumer };
