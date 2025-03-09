e28d77e7dd7da7740dae1ddc7ec9a5e0dd279026
Ben <128577990+bencoronard@users.noreply.github.com>
2025-03-09 12:01:08 +0700

    Initialize jpa entities

---
+++ src/main/java/dev/hireben/demo/rest/permission/infrastructure/persistence/jpa/entity/ApiAccessEntity.java
@@ -0,0 +1,9 @@
+package dev.hireben.demo.rest.permission.infrastructure.persistence.jpa.entity;
+
+public class ApiAccessEntity {
+
+  // ---------------------------------------------------------------------------//
+  // Fields
+  // ---------------------------------------------------------------------------//
+
+}
---
+++ src/main/java/dev/hireben/demo/rest/permission/infrastructure/persistence/jpa/entity/base/ResourceAccessEntity.java
@@ -0,0 +1,5 @@
+package dev.hireben.demo.rest.permission.infrastructure.persistence.jpa.entity.base;
+
+public abstract class ResourceAccessEntity {
+
+}
