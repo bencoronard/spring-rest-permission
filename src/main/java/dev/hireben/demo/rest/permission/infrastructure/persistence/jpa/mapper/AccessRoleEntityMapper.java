1179aa9fdf3e7781733e62f870eb806e794d08c0
Ben <128577990+bencoronard@users.noreply.github.com>
2025-03-10 13:48:15 +0700

    Add JPA entity mappers

---
+++ src/main/java/dev/hireben/demo/rest/permission/infrastructure/persistence/jpa/mapper/AccessRoleEntityMapper.java
@@ -0,0 +1,42 @@
+package dev.hireben.demo.rest.permission.infrastructure.persistence.jpa.mapper;
+
+import java.util.stream.Collectors;
+
+import dev.hireben.demo.rest.permission.domain.entity.AccessRole;
+import dev.hireben.demo.rest.permission.infrastructure.persistence.jpa.entity.AccessRoleEntity;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class AccessRoleEntityMapper {
+
+  // ---------------------------------------------------------------------------//
+  // Methods
+  // ---------------------------------------------------------------------------//
+
+  public AccessRoleEntity toEntity(AccessRole domain) {
+    return AccessRoleEntity.builder()
+        .name(domain.getName())
+        .viewAccesses(domain.getViewAccesses().stream()
+            .map(ViewAccessEntityMapper::toEntity)
+            .collect(Collectors.toSet()))
+        .apiAccesses(domain.getApiAccesses().stream()
+            .map(ApiAccessEntityMapper::toEntity)
+            .collect(Collectors.toSet()))
+        .build();
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public AccessRole toDomain(AccessRoleEntity entity) {
+    return AccessRole.builder()
+        .name(entity.getName())
+        .viewAccesses(entity.getViewAccesses().stream()
+            .map(ViewAccessEntityMapper::toDomain)
+            .collect(Collectors.toSet()))
+        .apiAccesses(entity.getApiAccesses().stream()
+            .map(ApiAccessEntityMapper::toDomain)
+            .collect(Collectors.toSet()))
+        .build();
+  }
+
+}
---
+++ src/main/java/dev/hireben/demo/rest/permission/infrastructure/persistence/jpa/mapper/ApiAccessEntityMapper.java
@@ -0,0 +1,38 @@
+package dev.hireben.demo.rest.permission.infrastructure.persistence.jpa.mapper;
+
+import java.util.stream.Collectors;
+
+import dev.hireben.demo.rest.permission.domain.entity.ApiAccess;
+import dev.hireben.demo.rest.permission.infrastructure.persistence.jpa.entity.ApiAccessEntity;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ApiAccessEntityMapper {
+
+  // ---------------------------------------------------------------------------//
+  // Methods
+  // ---------------------------------------------------------------------------//
+
+  public ApiAccessEntity toEntity(ApiAccess domain) {
+    return ApiAccessEntity.builder()
+        .path(domain.getName())
+        .token(domain.getToken())
+        .linkedViews(domain.getLinkedViews().stream()
+            .map(ViewAccessEntityMapper::toEntity)
+            .collect(Collectors.toSet()))
+        .build();
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public ApiAccess toDomain(ApiAccessEntity entity) {
+    return ApiAccess.builder()
+        .name(entity.getPath())
+        .token(entity.getToken())
+        .linkedViews(entity.getLinkedViews().stream()
+            .map(ViewAccessEntityMapper::toDomain)
+            .collect(Collectors.toSet()))
+        .build();
+  }
+
+}
