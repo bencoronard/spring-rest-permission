e389e93bea4a5bb2f82a197e04b170d24860083d
Ben <128577990+bencoronard@users.noreply.github.com>
2025-03-10 11:28:55 +0700

    Add assign API to View use cases

---
+++ src/main/java/dev/hireben/demo/rest/permission/application/usecase/AssignApiAccessToViewUseCase.java
@@ -0,0 +1,39 @@
+package dev.hireben.demo.rest.permission.application.usecase;
+
+import java.util.Collection;
+import java.util.Set;
+
+import dev.hireben.demo.rest.permission.application.exception.NonExistentViewAccessException;
+import dev.hireben.demo.rest.permission.domain.entity.ApiAccess;
+import dev.hireben.demo.rest.permission.domain.entity.ViewAccess;
+import dev.hireben.demo.rest.permission.domain.repository.ApiAccessRepository;
+import dev.hireben.demo.rest.permission.domain.repository.ViewAccessRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class AssignApiAccessToViewUseCase {
+
+  // ---------------------------------------------------------------------------//
+  // Fields
+  // ---------------------------------------------------------------------------//
+
+  private final ViewAccessRepository viewAccessRepository;
+  private final ApiAccessRepository apiAccessRepository;
+
+  // ---------------------------------------------------------------------------//
+  // Methods
+  // ---------------------------------------------------------------------------//
+
+  public void execute(String viewName, Collection<String> apiNames) {
+
+    ViewAccess view = viewAccessRepository.findByName(viewName)
+        .orElseThrow(() -> new NonExistentViewAccessException(String.format("View %s not found", viewName)));
+
+    Set<ApiAccess> apis = apiAccessRepository.findByNameIn(apiNames);
+
+    view.getLinkedApis().addAll(apis);
+
+    viewAccessRepository.save(view);
+  }
+
+}
