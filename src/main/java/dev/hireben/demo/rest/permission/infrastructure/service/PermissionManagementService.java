c1bfa90744684323189af3abad38aafc35ad3483
Ben <128577990+bencoronard@users.noreply.github.com>
2025-03-11 10:40:29 +0700

    Update PermissionManagementService.java

--- src/main/java/dev/hireben/demo/rest/permission/infrastructure/service/PermissionManagementService.java
+++ src/main/java/dev/hireben/demo/rest/permission/infrastructure/service/PermissionManagementService.java
@@ -1,9 +1,15 @@
 package dev.hireben.demo.rest.permission.infrastructure.service;
 
-import org.hibernate.sql.Delete;
+import java.util.Collection;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import dev.hireben.demo.rest.permission.application.dto.CreateAccessRoleDTO;
+import dev.hireben.demo.rest.permission.application.dto.CreateApiAccessDTO;
+import dev.hireben.demo.rest.permission.application.dto.CreateViewAccessDTO;
+import dev.hireben.demo.rest.permission.application.dto.UpdateApiAccessDTO;
+import dev.hireben.demo.rest.permission.application.dto.UpdateViewAccessDTO;
 import dev.hireben.demo.rest.permission.application.usecase.AssignApiAccessToRoleUseCase;
 import dev.hireben.demo.rest.permission.application.usecase.AssignApiAccessToViewUseCase;
 import dev.hireben.demo.rest.permission.application.usecase.AssignViewAccessToRoleUseCase;
@@ -24,7 +30,6 @@ import lombok.RequiredArgsConstructor;
 @Service
 @Transactional
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-@SuppressWarnings("unused")
 public class PermissionManagementService {
 
   // ---------------------------------------------------------------------------//
@@ -53,5 +58,87 @@ public class PermissionManagementService {
   // ---------------------------------------------------------------------------//
   // Methods
   // ---------------------------------------------------------------------------//
+
+  public void assignApiAccessToRole(String roleName, Collection<String> apiNames) {
+    assignApiAccessToRoleUseCase.execute(roleName, apiNames);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public void assignApiAccessToView(String viewName, Collection<String> apiNames) {
+    assignApiAccessToViewUseCase.execute(viewName, apiNames);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public void assignViewAccessToRole(String roleName, Collection<String> viewNames) {
+    assignViewAccessToRoleUseCase.execute(roleName, viewNames);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public void unassignApiAccessFromRole(String roleName, Collection<String> apiNames) {
+    unassignApiAccessFromRoleUseCase.execute(roleName, apiNames);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public void unassignApiAccessFromView(String viewName, Collection<String> apiNames) {
+    unassignApiAccessFromViewUseCase.execute(viewName, apiNames);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public void unassignViewAccessFromRole(String roleName, Collection<String> viewNames) {
+    unassignViewAccessFromRoleUseCase.execute(roleName, viewNames);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public String createAccessRole(CreateAccessRoleDTO dto) {
+    return createAccessRoleUseCase.execute(dto);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public String createApiAccess(CreateApiAccessDTO dto) {
+    return createApiAccessUseCase.execute(dto);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public String createViewAccess(CreateViewAccessDTO dto) {
+    return createViewAccessUseCase.execute(dto);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public void updateApiAccess(String apiName, UpdateApiAccessDTO dto) {
+    updateApiAccessUseCase.execute(apiName, dto);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public void updateViewAccess(String viewName, UpdateViewAccessDTO dto) {
+    updateViewAccessUseCase.execute(viewName, dto);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public void deleteAccessRole(String roleName) {
+    deleteAccessRoleUseCase.execute(roleName);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public void deleteApiAccess(String apiName) {
+    deleteApiAccessUseCase.execute(apiName);
+  }
+
+  // ---------------------------------------------------------------------------//
+
+  public void deleteViewAccess(String viewName) {
+    deleteViewAccessUseCase.execute(viewName);
+  }
 
 }
