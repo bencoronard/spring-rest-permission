b1413cd49f7a3fcd701b7d7fddfaf373d43444f7
Ben <128577990+bencoronard@users.noreply.github.com>
2025-03-12 09:54:16 +0700

    Update PermissionManagementControllerV1.java

--- src/main/java/dev/hireben/demo/rest/permission/presentation/controller/PermissionManagementControllerV1.java
+++ src/main/java/dev/hireben/demo/rest/permission/presentation/controller/PermissionManagementControllerV1.java
@@ -4,12 +4,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dev.hireben.demo.rest.permission.application.dto.CreateAccessRoleDTO;
+import dev.hireben.demo.rest.permission.application.dto.CreateApiAccessDTO;
+import dev.hireben.demo.rest.permission.application.dto.CreateViewAccessDTO;
+import dev.hireben.demo.rest.permission.application.dto.UpdateApiAccessDTO;
+import dev.hireben.demo.rest.permission.application.dto.UpdateViewAccessDTO;
 import dev.hireben.demo.rest.permission.infrastructure.service.PermissionManagementService;
+import dev.hireben.demo.rest.permission.presentation.dto.CreateApiAccessRequest;
+import dev.hireben.demo.rest.permission.presentation.dto.CreateRoleRequest;
+import dev.hireben.demo.rest.permission.presentation.dto.CreateViewAccessRequest;
+import dev.hireben.demo.rest.permission.presentation.dto.UpdateApiAccessRequest;
+import dev.hireben.demo.rest.permission.presentation.dto.UpdateViewAccessRequest;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/v1/permissions")
@@ -69,36 +84,86 @@ public class PermissionManagementControllerV1 {
 
   // ---------------------------------------------------------------------------//
 
+  @PostMapping("/roles")
   public ResponseEntity<Void> createRole(
-      @PathVariable String roleName) {
+      @RequestBody @Valid CreateRoleRequest body) {
+
+    CreateAccessRoleDTO dto = CreateAccessRoleDTO.builder()
+        .roleName(body.roleName())
+        .apiNames(body.apiNames())
+        .viewNames(body.viewNames())
+        .build();
+
+    permissionManagementService.createAccessRole(dto);
+
     return ResponseEntity.ok().build();
   }
 
   // ---------------------------------------------------------------------------//
 
+  @PostMapping("/apis")
   public ResponseEntity<Void> createApiPermission(
-      @PathVariable String apiName) {
+      @RequestBody @Valid CreateApiAccessRequest body) {
+
+    CreateApiAccessDTO dto = CreateApiAccessDTO.builder()
+        .apiName(body.apiName())
+        .apiToken(body.apiToken())
+        .linkedViewNames(body.linkedViewNames())
+        .build();
+
+    permissionManagementService.createApiAccess(dto);
+
     return ResponseEntity.ok().build();
   }
 
   // ---------------------------------------------------------------------------//
 
+  @PostMapping("/views")
   public ResponseEntity<Void> createViewPermission(
-      @PathVariable String viewName) {
+      @RequestBody @Valid CreateViewAccessRequest body) {
+
+    CreateViewAccessDTO dto = CreateViewAccessDTO.builder()
+        .viewName(body.viewName())
+        .viewToken(body.viewToken())
+        .linkedApiNames(body.linkedApiNames())
+        .build();
+
+    permissionManagementService.createViewAccess(dto);
+
     return ResponseEntity.ok().build();
   }
 
   // ---------------------------------------------------------------------------//
 
+  @PatchMapping("/apis/{apiName}")
   public ResponseEntity<Void> updateApiPermission(
-      @PathVariable String apiName) {
+      @PathVariable String apiName,
+      @RequestBody @Valid UpdateApiAccessRequest body) {
+
+    UpdateApiAccessDTO dto = UpdateApiAccessDTO.builder()
+        .newApiName(body.newApiName())
+        .newApiToken(body.newApiToken())
+        .build();
+
+    permissionManagementService.updateApiAccess(apiName, dto);
+
     return ResponseEntity.ok().build();
   }
 
   // ---------------------------------------------------------------------------//
 
+  @PatchMapping("/views/{viewName}")
   public ResponseEntity<Void> updateViewPermission(
-      @PathVariable String viewName) {
+      @PathVariable String viewName,
+      @RequestBody @Valid UpdateViewAccessRequest body) {
+
+    UpdateViewAccessDTO dto = UpdateViewAccessDTO.builder()
+        .newViewName(body.newViewName())
+        .newViewToken(body.newViewToken())
+        .build();
+
+    permissionManagementService.updateViewAccess(viewName, dto);
+
     return ResponseEntity.ok().build();
   }
 
